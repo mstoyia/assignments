cf79e663f9f6f9cf6469ecdd583c629eeed079db
Toyia Joseph <toyia0@gmail.com>
2024-03-20 16:23:48 -0400

    Finished lab 2 and made corrections on change project

--- labs/Triangle.java
+++ labs/Triangle.java
@@ -1,5 +1,27 @@
-package labs;
+//package labs;
+
+import java.util.Scanner;
 
 public class Triangle {
-    
+    public static double calculateArea( double baseWidth, double height) {
+      double area = baseWidth * height / 2; 
+        return  
+        area;
+
+    }
+
+   public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+            System.out.println("Enter the baseWidth of the triangle: ");
+            double baseWidth = input.nextDouble();
+
+            System.out.println("Enter the height of the triangle: ");
+            double height = input.nextDouble();
+
+            System.out.println("The area of the triangle is: " + baseWidth *  height / 2);
+             
+            
+            
+
+   }
 }
