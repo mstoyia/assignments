773e2fcd25c97d779c5eaffec1fd0680012d8407
Toyia Joseph <toyia0@gmail.com>
2024-03-22 10:10:50 -0400

    Finished number 3 of lab 1

---
+++ labs/EvenOrOdd.java
@@ -0,0 +1,23 @@
+//package labs;
+
+import java.util.Scanner;
+
+public class EvenOrOdd {
+    public static String evenorOdd(int number) {
+        if (number % 4 == 0) {
+            return "The number is even";
+        } else {
+            return "The number is odd";
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the number you want to evaluate: ");
+        int num = input.nextInt();
+        String result = evenorOdd(num);
+        System.out.println(result);
+
+    }
+    
+}
