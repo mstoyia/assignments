eb3a707e57d88440440600418f9d5c4e8648dc82
Toyia Joseph <toyia0@gmail.com>
2024-03-30 19:18:36 -0400

    Completed some more class assignments

---
+++ labs/Person.java
@@ -0,0 +1,10 @@
+public abstract class Person {
+    String firstName;
+    String lastName;
+    
+    public abstract static double taxes(double taxes) {
+        return double taxes;
+    }
+    
+}
+
